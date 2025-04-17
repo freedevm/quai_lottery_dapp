720abbc8bdce00dd3d307670626bc7f5a939d271
0806SunnyDev <lightdev05@gmail.com>
2025-04-17 14:50:56 -0700

    fix screen height issue

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,7 +5,7 @@ import Header from "./_components/Header";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col -h-screen bg-gradient-to-b from-purple-900 to-black text-white">
+    <div className="flex flex-col min-h-screen bg-gradient-to-b from-purple-900 to-black text-white">
       <Header />
       <div className="pt-12 sm:pt-16 md:pt-20 pb-28 lg:pb-4 flex-auto">
         {children}
